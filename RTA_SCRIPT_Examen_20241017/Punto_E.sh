a8e686a27968416644aa3a06792a203b908d69c4
RamiroZucco <zuccoramiro@gmail.com>
2024-10-18 00:13:11 +0000

    Filtros Basicos Punto E realizado

---
+++ RTA_SCRIPT_Examen_20241017/Punto_E.sh
@@ -0,0 +1,4 @@
+grep MemTotal /proc/meminfo
+sudo grep MemTotal /proc/meminfo > Filtro_Basico.txt
+sudo dmidecode -t chassis
+sudo dmidecode -t chassis | grep -A1 "Chassis Information" > Filtro_Basico.txt
